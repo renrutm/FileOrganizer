ede9108a81f4e7b921b96ce406b39ce3d5b6dcd7
miketurner <mike@noumenadigital.com>
2022-02-05 13:19:11 +0100

    changes

--- src/main/resources/static/javascript/mainmvc.js
+++ src/main/resources/static/javascript/mainmvc.js
@@ -46,33 +46,86 @@ function addNode(nodeValue) {
 
     if(nodeValue.parentId !== "") { //root
         obj.parentId = nodeValue.parentId
+    } else {
+        this.nodes.root = obj;
     }
 
     if(this[nodeValue.parentId] === undefined) { //parent doesn't exist yet
         this[nodeValue.parentId] = {};
         this[nodeValue.parentId].children = [];
-    }
-
-    this[nodeValue.id] = obj
-    this[nodeValue.parentId].children.push(this[nodeValue.id]);
+    } //else {
+        this[nodeValue.id] = obj
+        this[nodeValue.parentId].children.push(this[nodeValue.id]);
+        // this[nodeValue.parentId].children.push(obj);
+    // }
 }
 
 function findRootNode(nodeValue, index, arr) {
     return nodeValue.parentId === "";
 }
 
+function buildNodeTree(dom, node) {
+    // find root
 
-function buildHtmlTree(nodeTree) {
-
+    if(node.children.length > 0) {
+        addNodeWithChildrenHtml(dom, node)
+    } else {
+        addChildlessNodeHtml(dom, node)
+    }
     /*
-        find root node
-            add parent node
-            lopp through
-     */
+            if(children) // branch
+                addNodeWithChildrenHtml(rootDom, root)
 
+            else // leaf
+               addChildlessNodeHtml(rootDom, root)
+     */
 
 }
 
-function addParentHtmlNode(currentDom, node) {
-    currentDon.
+function addNodeWithChildrenHtml(parentDom, node) {
+
+    parentDom.prepend(getParentContainerHtml(node));
+    for(const child in node.children) {
+        if(child.children.length > 0) {
+            addNodeWithChildrenHtml(parentDom, child)
+        } else {
+            addChildlessNodeHtml(parentDom, child)
+        }
+    }
+
+
+    /*
+        addparentHtml(node)
+        loop through children
+            if child has children
+                addNodeWithChildrenHtml(parent)
+            else
+                addChildlessNodeHtml(parentDom, childnode)
+    /*
+    <a href="javascript:void(0);" class="list-group-item" data-toggle="collapse">
+        <i class="fa fa-chevron"></i>
+        <span class="badge">1</span>
+        Item 1
+    </a>
+    <div class="list-group collapse">
+    </div>
+     */
+}
+function getParentContainerHtml(node) {
+    const html = "<a href=\"javascript:void(0);\" class=\"list-group-item\" data-toggle=\"collapse\">\n" +
+        "        <i class=\"fa fa-chevron\"></i>\n" +
+        "        <span class=\"badge\">1</span>\n" +
+        "        ${node.fileName}\n" +
+        "    </a>\n" +
+        "    <div id=\"${node.id}\" class=\"list-group collapse\">\n" +
+        "    </div>"
+    return html;
+}
+
+function addChildlessNodeHtml(parentDom, node) {
+    const html = "<a href=\"javascript:void(0);\" class=\"list-group-item\"><span class=\"badge\">0</span>${node.fileName}</a>\n";
+    return html;
+    /*
+        <a href="javascript:void(0);" class="list-group-item"><span class="badge">0</span>Item 1.1.1</a>
+     */
 }
